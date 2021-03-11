223bbd1e339aea15f46780e00f7292f43e78c3e3
piyushsi <piyushsinha2309@gmail.com>
2021-03-11 12:17:16 +0530

    error handled

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,9 +6,12 @@ class Link < ActiveRecord::Base
     validates :url, presence: true, length: { maximum: 255 }, format: URI::regexp(%w[http https])
   
     def generate_slug
+      if slug
+      else
       loop do
         self.slug = [ *(SecureRandom.random_number(10)..SecureRandom.random_number(10)),*("a".."z")].shuffle[0, 5].join
         break slug unless Link.where(slug: self.slug).exists?
       end
     end
+  end
   end
